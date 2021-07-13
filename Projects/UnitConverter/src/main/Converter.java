c3218c71260f39080f7c603300d524e186a7dc3e
Miguel Partida <mig21.ap@gmail.com>
2021-07-13 08:21:29 -0700

    Started
    Added project and folders

---
+++ Projects/UnitConverter/src/main/Converter.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Scanner;
+
+public class Converter {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int menuSelection =0;
+		
+		while(menuSelection != 4) {
+			 System.out.println("Please select an option:\n"
+					 + "1. Cups to Teaspoons\n"
+					 + "2. Miles to Kilometers\n"
+					 + "3. Us Gallons to Imperial Gallons\n"
+					 + "4. Quit");
+			 menuSelection = scanner.nextInt();
+		}
+	}
+}
