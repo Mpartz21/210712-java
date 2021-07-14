205ec9e47f50aea9cc7ed365231e5d3c199d1fe6
MPartz21 <mig21.ap@gmail.com>
2021-07-13 17:02:05 -0700

    Lab HelloWorld

--- Projects/UnitConverter/src/main/Converter.java
+++ Projects/UnitConverter/src/main/Converter.java
@@ -6,7 +6,7 @@ public class Converter {
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int menuSelection =0;
+		int menuSelection = 0;
 		
 		while(menuSelection != 4) {
 			 System.out.println("Please select an option:\n"
@@ -15,6 +15,24 @@ public class Converter {
 					 + "3. Us Gallons to Imperial Gallons\n"
 					 + "4. Quit");
 			 menuSelection = scanner.nextInt();
+			 switch(menuSelection) {
+			 case 1:
+				 
+			 case 2:
+				 
+			 case 3:
+				 
+			 case 4:
+				 
+			 default:
+					 
+			 }
 		}
+	} 
+	
+	
+	public void cupsToTeaspoons(double cups) {
+		
 	}
+	
 }
