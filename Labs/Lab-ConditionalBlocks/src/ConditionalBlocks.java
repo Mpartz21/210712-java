36d57c48b0dafae6bee6529563f48ead448077f4
MPartz21 <mig21.ap@gmail.com>
2021-07-14 09:44:10 -0700

    Lab Conditional Blocks

---
+++ Labs/Lab-ConditionalBlocks/src/ConditionalBlocks.java
@@ -0,0 +1,14 @@
+
+public class ConditionalBlocks {
+	
+	public static void main(String[] args) {  
+		boolean bool = true;  
+		
+		if (bool){  
+			System.out.println("inside the if-statement");  
+		}
+		
+		System.out.println("Outside of the if-statement");  
+	}  
+}
+
